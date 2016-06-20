36de61e3ef225f143d6aef50be3635d1710a062f
Gerald <gera2ld@163.com>
2016-06-20 11:24:13 +0800

    fix bin script

--- README.md
+++ README.md
@@ -1,18 +1,42 @@
 spa-static
 ===
 
+![NPM](https://img.shields.io/npm/v/spa-static.svg)
+![License](https://img.shields.io/npm/l/spa-static.svg)
+![Downloads](https://img.shields.io/npm/dt/spa-static.svg)
+
 A web server to serve static files for SPAs.
+
+Installation
+---
+``` sh
+$ npm i spa-static
+```
 
 Usage
 ---
-``` sh
-$ PORT=4002 node .
+Via JavaScript:
+``` node
+require('spa-static')({
+  host: 'localhost',
+  port: 4002,
+}, (e, callback) => {
+  if (e) throw e;
+  console.log('Listening...');
+});
 ```
 
-Environment variables
+Via bash:
+``` sh
+$ HOST=localhost PORT=4002 spa-static
+```
+
+Options
 ---
-* HOST
-* PORT
-* PREFIX
-* STATIC
-* INDEX
+JavaScript options can be mapped to environment variables for running through scripts.
+
+* HOST -> options.host: default as `localhost`
+* PORT -> options.port: default as `4000`
+* PREFIX -> options.prefix: default as `''`
+* STATIC -> options.staticDir: default as `./static`
+* INDEX -> options.index: default as `/index.html`
--- index.js
+++ index.js
@@ -2,37 +2,44 @@ const koa = require('koa');
 const send = require('koa-send');
 const compress = require('koa-compress');
 const etag = require('koa-etag');
-const env = process.env;
 
-const port = env.PORT || 4000;
-const host = env.HOST == null ? 'localhost' : env.HOST;
-const prefix = env.PREFIX;
-const staticDir = env.STATIC || 'static';
-const index = env.INDEX || '/index.html';
-const staticOptions = {
-  root: staticDir,
-};
-
-const app = koa();
-
-app
-.use(compress())
-.use(function* (next) {
-  yield next;
-  const etag = this.get('If-None-Match');
-  if (this.status === 200 && etag && etag === this.response.get('ETag')) {
-    this.status = 304;
-    this.body = '';
+module.exports = function (args, callback) {
+  if (typeof args === 'function') {
+    callback= args;
+    args = {};
+  } else {
+    args = args || {};
   }
-})
-.use(etag())
-.use(function* (next) {
-  // this.set('Access-Control-Allow-Origin', '*');
-  const path = prefix && this.path.startsWith(prefix) ? this.path.slice(prefix.length) : this.path;
-  const filepath = yield send(this, path, staticOptions);
-  if (!filepath) yield send(this, index, staticOptions);
-})
-.listen(port, host, e => {
-  if (e) console.error(e);
-  else console.log(`Listening at ${host}:${port}...`);
-});
+
+  args.port = args.port || 4000;
+  if (args.host == null) args.host = 'localhost';
+  args.staticDir = args.staticDir || 'static';
+  args.index = args.index || '/index.html';
+  const staticOptions = {
+    root: args.staticDir,
+  };
+
+  const app = koa();
+
+  app
+  .use(compress())
+  .use(function* (next) {
+    yield next;
+    const etag = this.get('If-None-Match');
+    if (this.status === 200 && etag && etag === this.response.get('ETag')) {
+      this.status = 304;
+      this.body = '';
+    }
+  })
+  .use(etag())
+  .use(function* (next) {
+    // this.set('Access-Control-Allow-Origin', '*');
+    const path = args.prefix && this.path.startsWith(args.prefix) ? this.path.slice(args.prefix.length) : this.path;
+    const filepath = yield send(this, path, staticOptions);
+    if (!filepath) yield send(this, args.index, staticOptions);
+  });
+
+  const server = app.listen(args.port, args.host, e => {
+    callback(e, server);
+  });
+};
